8b8f810658ba9be5d33eaed083b829a7b60a06a8
Arturo <arturo.drago@gmail.com>
2024-01-31 21:49:51 -0600

    Post-It

---
+++ Post-Its/Post-It.js
@@ -0,0 +1,27 @@
+const addBt = document.getElementById("add-button");
+const notes = document.getElementById("sticky-notes");
+const noteText = document.getElementById("text");
+const Note = document.getElementsByClassName("li");
+
+const colors = ["red", "yellow", "blue", "green", "orange", "turquoise"];
+
+function addStickyNote(){
+    if(noteText.value != ''){
+        let c = notes.childNodes.length % 6;
+        const note = document.createElement("li");
+        note.innerHTML = noteText.value;
+        note.style.backgroundColor = colors[c];
+        if(c % 2 == 1){
+            note.style.color = "black";
+        }
+        notes.appendChild(note);
+    }else{
+        alert("No se puede crear una nota vacia");
+    }
+}
+
+function editNote(){
+    alert("im here");
+}
+
+addBt.addEventListener('click', addStickyNote);
