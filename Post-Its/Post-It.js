916cafae555da73ac123ea71a816f53973f93fe5
Arturo <arturo.drago@gmail.com>
2024-02-03 16:32:40 -0600

    Post-It 3

--- Post-Its/Post-It.js
+++ Post-Its/Post-It.js
@@ -1,27 +1,39 @@
 const addBt = document.getElementById("add-button");
-const notes = document.getElementById("sticky-notes");
 const noteText = document.getElementById("text");
-const Note = document.getElementsByClassName("li");//Lo iba a usar con el boton para editar al igual que la funcion editNote
+const stickyNotes = document.getElementById("list");
+const newListDiv = document.querySelector(".newList");
 
 const colors = ["red", "yellow", "blue", "green", "orange", "turquoise"];
 
 function addStickyNote(){
-    if(noteText.value != ''){
-        let c = notes.childNodes.length % 6;
-        const note = document.createElement("li");
-        note.innerHTML = noteText.value;
-        note.style.backgroundColor = colors[c];
-        if(c % 2 == 1){
-            note.style.color = "black";
-        }
-        notes.appendChild(note);
-    }else{
-        alert("No se puede crear una nota vacia");
+    const removeButton = document.getElementById("add-button");
+    var notesSize = stickyNotes.getElementsByTagName('*').length;
+    let c = (notesSize-1) % 6;
+    
+    removeButton.parentNode.removeChild(removeButton);
+
+    const note = document.createElement("button");
+    note.innerText = noteText.value;
+    note.id = "postIt";
+    note.style.backgroundColor = colors[c];
+    if(c % 2 == 1){
+        note.style.color = "black";
     }
+
+    let addButton = document.createElement("button");
+    addButton.innerText = "+";
+    addButton.id = "add-button";
+
+    stickyNotes.appendChild(note);
+    stickyNotes.appendChild(addButton);
 }
 
-function editNote(){
-    alert("im here");
-}
 
-addBt.addEventListener('click', addStickyNote);
+//Hace que funcione el boton a pesar de ser eliminado y creado de nuevo con cada nota agregada
+const div = document.getElementsByTagName("div")[0];
+
+div.addEventListener("click", (event) => {
+  if(event.target.id === 'add-button') {
+    addStickyNote();
+  }
+})
